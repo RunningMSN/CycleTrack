b9c12a831cb98019508a321b40d8d8d5770ac822
RunningMSN <running.msn.wi@gmail.com>
2022-01-26 11:20:46 -0600

    Began adding cycle tracking

--- website/models.py
+++ website/models.py
@@ -5,3 +5,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+    cycles = db.relationship('Cycle')
+
+class Cycle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cycle_year = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- website/pages.py
+++ website/pages.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import current_user, login_required
-from . import db
+from . import db, site_settings
+from .models import Cycle
 
 pages = Blueprint('pages', __name__)
 
@@ -8,7 +9,13 @@ pages = Blueprint('pages', __name__)
 def index():
     return render_template('index.html', user=current_user)
 
-@pages.route('/dashboard')
+@pages.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html', user=current_user)
+    # Manage all requests for updating information
+    if request.method == 'POST':
+        add_cycle = request.form.get('add_cycle')
+        db.session.add(Cycle(cycle_year=int(add_cycle), user_id=current_user.id))
+        db.session.commit()
+        flash('Cycle added.', category='success')
+    return render_template('dashboard.html', user=current_user, cycles=site_settings.VALID_CYCLES)
