b4376e0c59f051b7216d8adcaf7b8e41e3c24a13
RunningMSN <running.msn.wi@gmail.com>
2022-01-27 11:56:34 -0600

    Added school model

--- website/models.py
+++ website/models.py
@@ -11,3 +11,17 @@ class Cycle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cycle_year = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    schools = db.relationship('School')
+
+class School(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cycle = db.Column(db.Integer, db.ForeignKey('cycle.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    primary = db.Column(db.DateTime(timezone=True), nullable=True)
+    secondary_received = db.Column(db.DateTime(timezone=True), nullable=True)
+    application_complete = db.Column(db.DateTime(timezone=True), nullable=True)
+    interview_received = db.Column(db.DateTime(timezone=True), nullable=True)
+    interview_date = db.Column(db.DateTime(timezone=True), nullable=True)
+    rejection = db.Column(db.DateTime(timezone=True), nullable=True)
+    waitlist = db.Column(db.DateTime(timezone=True), nullable=True)
+    acceptance = db.Column(db.DateTime(timezone=True), nullable=True)
--- website/site_settings.py
+++ website/site_settings.py
@@ -1,3 +1,4 @@
 DB_NAME = 'database.db'
 SECRET_KEY = 'my_$s3cr3t_k3y'
 VALID_CYCLES = [2022, 2021, 2020]
+SCHOOL_LIST = ["Test 1", "Test 2", "Test 3"]
