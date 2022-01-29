b6c748c87beb4c4629cdc04693f8ce5d7d0045ce
RunningMSN <running.msn.wi@gmail.com>
2022-01-29 14:34:50 -0600

    Moved lists of options for HTML forms into their own file

---
+++ website/form_options.py
@@ -0,0 +1,30 @@
+# Cycles to include in site
+VALID_CYCLES = [2022, 2021, 2020]
+
+# Valid schools to add into school list
+SCHOOL_LIST = sorted(
+    ["Alabama", "South Alabama", "Arkansas", "Arizona", "Arizona Phoenix", "California", "California Northstate",
+     "Kaiser Permanente", "Loma Linda", "Southern Cal-Keck", "Stanford", "UC Davis", "UC Irvine", "UC Riverside",
+     "UC San Diego", "UC San Francisco", "UCLA-Geffen", "Colorado", "Connecticut", "Quinnipiac-Netter", "Yale",
+     "George Washington", "Georgetown", "Howard", "FIU-Wertheim", "Florida", "Florida Atlantic-Schmidt",
+     "Florida State", "Miami-Miller", "Nova Southeastern-Patel", "UCF", "USF-Morsani", "Emory", "MC Georgia Augusta",
+     "Mercer", "Morehouse", "Hawaii-Burns", "Iowa-Carver", "Carle Illinois", "Chicago Med Franklin", "Chicago-Pritzker",
+     "Illinois", "Loyola-Stritch", "Northwestern-Feinberg", "Rush", "Southern Illinois", "Indiana", "Kansas",
+     "Kentucky", "Louisville", "LSU New Orleans", "LSU Shreveport", "Tulane", "Boston", "Harvard", "Massachusetts",
+     "Tufts", "Johns Hopkins", "Maryland", "Uniformed Services-Hebert", "Central Michigan", "Michigan",
+     "Michigan State", "Oakland Beaumont", "Wayne State", "Western Michigan-Stryker", "Mayo", "Minnesota",
+     "Missouri Columbia", "Missouri Kansas City", "Saint Louis", "Washington U St Louis", "Mississippi", "Duke",
+     "East Carolina-Brody", "North Carolina", "Wake Forest", "North Dakota", "Creighton", "Nebraska",
+     "Dartmouth-Geisel", "Cooper Rowan", "Hackensack Meridian", "Rutgers New Jersey", "Rutgers-RW Johnson",
+     "New Mexico", "Nevada Reno", "UNLV-Kerkorian", "Albany", "Buffalo-Jacobs", "CUNY", "Columbia-Vagelos",
+     "Cornell-Weill", "Einstein", "Mount Sinai-Icahn", "NYU Long Island", "NYU-Grossman", "New York Medical",
+     "Renaissance Stony Brook", "Rochester", "SUNY Downstate", "SUNY Upstate", "Zucker Hofstra Northwell",
+     "Case Western Reserve", "Cincinnati", "Northeast Ohio", "Ohio State", "Toledo", "Wright State-Boonshoft",
+     "Oklahoma", "Oregon", "Drexel", "Geisinger Commonwealth", "Jefferson-Kimmel", "Penn State",
+     "Pennsylvania-Perelman", "Pittsburgh", "Temple-Katz", "Caribe", "Ponce", "Puerto Rico", "San Juan Bautista",
+     "Brown-Alpert", "MU South Carolina", "South Carolina Columbia", "South Carolina Greenville",
+     "South Dakota-Sanford", "East Tennessee-Quillen", "Meharry", "Tennessee", "Vanderbilt", "Baylor", "Houston",
+     "TCU UNTHSC", "Texas A & M", "Texas Tech", "Texas Tech-Foster", "UT Austin-Dell", "UT Houston-McGovern",
+     "UT Medical Branch", "UT Rio Grande Valley", "UT San Antonio-Long", "UT Southwestern", "Utah", "Eastern Virginia",
+     "Virginia", "Virginia Commonwealth", "Virginia Tech Carilion", "Vermont-Larner", "U Washington",
+     "Washington State-Floyd", "MC Wisconsin", "Wisconsin", "Marshall-Edwards", "West Virginia"])
