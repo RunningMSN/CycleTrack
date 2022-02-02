8dfab204c52f87cbc7729d80bdeb8f3b2157a01b
RunningMSN <running.msn.wi@gmail.com>
2022-02-02 10:12:10 -0600

    Rename df for consistency

--- website/visualizations/dot.py
+++ website/visualizations/dot.py
@@ -2,9 +2,9 @@ import plotly
 import plotly.express as px
 import json
 
-def generate(data):
+def generate(cycle_data):
     '''Returns JSON for plotly dot graph of the application cycle.'''
-    melted = data.melt(id_vars=data.columns[0], value_vars=data.columns[1:], var_name='Actions', value_name='date')
+    melted = cycle_data.melt(id_vars=cycle_data.columns[0], value_vars=cycle_data.columns[1:], var_name='Actions', value_name='date')
     fig = px.scatter(melted, x='date', y='name', color='Actions',
                      labels={
                          'date': 'Date',
