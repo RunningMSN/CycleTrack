706953a94aa5e8557d6f19ad87579215b52cb695
RunningMSN <running.msn.wi@gmail.com>
2022-02-07 19:06:10 -0600

    Updated to use unified color palette and names for actions

--- website/visualizations/dot.py
+++ website/visualizations/dot.py
@@ -1,15 +1,26 @@
 import plotly
 import plotly.express as px
 import json
+from . import converters
 
 def generate(cycle_data):
     '''Returns JSON for plotly dot graph of the application cycle.'''
+    # Melt data
     melted = cycle_data.melt(id_vars=cycle_data.columns[0], value_vars=cycle_data.columns[1:], var_name='Actions', value_name='date')
+    # Generate scatterplot
     fig = px.scatter(melted, x='date', y='name', color='Actions',
                      labels={
                          'date': 'Date',
                          'name':''
                      },
-                     title="Application Cycle")
+                     title="Application Cycle",
+                     height=len(melted['name'].unique())*20) # Adjust height based on number of schools
+    # Update names of traces to make more readable
+    fig.for_each_trace(lambda t: t.update(name=converters.action_names[t.name],
+                                          legendgroup=converters.action_names[t.name],
+                                          hovertemplate=t.hovertemplate.replace(t.name, converters.action_names[t.name])
+                                          )
+                       )
+    # Generate JSON for plotting
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
