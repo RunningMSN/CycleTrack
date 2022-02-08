8e0dca3a3c93e597d74423fc133688abb4445f97
RunningMSN <running.msn.wi@gmail.com>
2022-02-07 18:50:00 -0600

    Updated to use unified color palette and names for actions

--- website/visualizations/bar.py
+++ website/visualizations/bar.py
@@ -4,8 +4,22 @@ import json
 from . import converters
 
 def generate(cycle_data):
+    # Generate dataframe for plotly
     cycle_data = converters.convert_bar_df(cycle_data)
-    fig = px.bar(cycle_data, x='Date', y='Count', color='Best Outcome')
+    fig = px.bar(cycle_data, x='Date',
+                 y='Count',
+                 color='Best Outcome',
+                 color_discrete_map=converters.fig_colors)
+    # Remove outlines
     fig.update_traces(marker=dict(line=dict(width=0)))
+    # Label using more readable action names
+    fig.for_each_trace(lambda t: t.update(name=converters.action_names[t.name],
+                                          legendgroup=converters.action_names[t.name],
+                                          hovertemplate=t.hovertemplate.replace(t.name, converters.action_names[t.name])
+                                          )
+                       )
+    # Remove gaps between bars
+    fig.update_layout(bargap=0)
+    # Convert to JSON for plotting
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
