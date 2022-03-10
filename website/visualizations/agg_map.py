7a40463568a05e4b9be4ed1cc2e73cdc0c6f6e50
ijhua <13755787+ijhua@users.noreply.github.com>
2022-03-10 14:18:38 -0800

    add URLs from the aggregate map to school-specific explorer pages and fix the aggregate map marker scaling

--- website/visualizations/agg_map.py
+++ website/visualizations/agg_map.py
@@ -4,11 +4,11 @@ import json
 import numpy as np
 from . import converters
 
-def scaler(dfseries,r):
-    min = dfseries.min()
-    max = dfseries.max()
-    range = max - min
-    new_series = (((dfseries - min)/range)*(r-1)+1)*6
+def scaler(dfseries,max_num,min_num):
+    new_series = []
+    for i in dfseries:
+        new_val = ((i - dfseries.min()) / (dfseries.max() - dfseries.min())) * (max_num - min_num) + min_num
+        new_series.append(new_val)
     return new_series
 
 
@@ -17,13 +17,7 @@ def generate(data):
     loc_df = df.groupby(["School","Long","Lat"]).size().reset_index()
     loc_df = loc_df.rename(columns={0:"Count"})
     
-    max_num = loc_df["Count"].max()
-    min_num = loc_df["Count"].min()
-
-    max_size = 30
-    min_size = 1
-
-    marker_scale = (max_size - min_size)/(max_num - min_num) + min_size
+    urls = ["/explorer/"+i for i in loc_df.School]
 
     fig = go.Figure()
     data = go.Scattergeo(
@@ -31,13 +25,15 @@ def generate(data):
             lat = loc_df["Lat"],
             text = loc_df["School"] + "<br>Applications: " + (loc_df["Count"]).astype(str),
             hoverinfo="text",
-            marker = dict(size = (scaler(loc_df["Count"],marker_scale)))
+            marker = dict(size = (scaler(loc_df["Count"],20,3))),
+            customdata = urls,
         )
     fig.add_trace(data)
     fig.update_layout(
         geo_scope = 'usa',
         height = 500,
-        margin = dict(l=25,r=25,t=25,b=25)
+        margin = dict(l=25,r=25,t=25,b=25),
+        clickmode="event"
     )
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
