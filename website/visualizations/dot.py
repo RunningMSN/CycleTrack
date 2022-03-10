2f10322dc32d65bee8d13d68d1cc41f44fd49553
ijhua <13755787+ijhua@users.noreply.github.com>
2022-03-10 15:40:10 -0800

    rewrite dotplot using graph objects and create opportunity to use symbols in the future

--- website/visualizations/dot.py
+++ website/visualizations/dot.py
@@ -1,14 +1,29 @@
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import json
 from . import converters
 
-def generate(cycle_data, title,color="default"):
-    '''Returns JSON for plotly dot graph of the application cycle.'''
+#leaving this for now in case we want to use it in the future
+symbol_map = {
+    'primary': 'circle', 
+    'secondary_received': 'square', 
+    'application_complete': 'diamond',
+    'interview_received':'cross', 
+    'interview_date': 'x', 
+    'rejection': 'triangle-up',
+    'waitlist': 'pentagon',
+    'acceptance': 'hourglass', 
+    'withdrawn': 'star'
+    }
+
+'''def generate1(cycle_data, title,color="default"):
+    #Returns JSON for plotly dot graph of the application cycle.
     # Melt data
     melted = cycle_data.melt(id_vars=cycle_data.columns[0], value_vars=cycle_data.columns[1:], var_name='Actions', value_name='date')
+    melted["symbols"] = melted["Actions"].map(symbol_map)
     # Generate scatterplot
-    fig = px.scatter(melted, x='date', y='name', color='Actions',
+    fig = px.scatter(melted, x='date', y='name', color='Actions', symbol = "symbols",
                      labels={
                          'date': 'Date',
                          'name':''
@@ -19,7 +34,7 @@ def generate(cycle_data, title,color="default"):
     # Update names of traces to make more readable
     fig.for_each_trace(lambda t: t.update(name=converters.action_names[t.name],
                                           legendgroup=converters.action_names[t.name],
-                                          hovertemplate=t.hovertemplate.replace(t.name, converters.action_names[t.name])
+                                          hovertemplate=t.hovertemplate.replace(t.name, converters.action_names[t.name]),
                                           )
                        )
     fig.update_layout(margin=dict(l=20, r=20, t=40, b=20))
@@ -32,5 +47,43 @@ def generate(cycle_data, title,color="default"):
         sizex=0.2, sizey=0.2,
         xanchor="right", yanchor="bottom"))
     # Generate JSON for plotting
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON'''
+
+def generate(cycle_data, title,color="default"):
+    melted = cycle_data.melt(id_vars=cycle_data.columns[0], value_vars=cycle_data.columns[1:], var_name='Actions', value_name='date')
+    
+    actions = melted["Actions"].unique()
+    
+    fig = go.Figure()
+    for action in actions:
+        df = melted.loc[melted["Actions"]==action]
+        fig.add_trace(go.Scatter(
+            x = df["date"],
+            y = df["name"],
+            mode = "markers",
+            name = converters.action_names[action],
+            hoverinfo="none",
+            marker = dict(
+                color=converters.palette[color][action],
+                size=10,
+                #symbol = symbol_map[action],
+                ),
+            text = df["name"]+"<br>"+ converters.action_names[action],
+            hovertemplate = "%{text}"+"<br>%{x}<extra></extra>",
+        ))
+    fig.update_layout(height=len(melted['name'].unique())*20,
+    title=title,
+    yaxis_type="category",
+    margin=dict(l=20, r=20, t=40, b=20))
+    fig.add_layout_image(
+            dict(
+            source="./static/images/CycleTrack_Plot_Watermark.png",
+            xref="x domain",
+            yref="y domain",
+            x=1, y=1,
+            sizex=0.2, sizey=0.2,
+            xanchor="right", yanchor="bottom"))
+
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
