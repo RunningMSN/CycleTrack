57628edc5e0dbb9949d7700bfc22936791097a9c
RunningMSN <running.msn.wi@gmail.com>
2022-03-11 16:10:43 -0600

    Fix some errors with empty rows imported from google sheets

--- website/helpers/import_list_funcs.py
+++ website/helpers/import_list_funcs.py
@@ -19,16 +19,22 @@ def read_google(link):
             # List of elements in the row
             row_data = []
             for element in row.find_all('td'):
-                row_data.append(element.get_text())
+                if len(element.get_text()) > 0:
+                    row_data.append(element.get_text())
+                else:
+                    row_data.append(None)
             input_data.append(row_data)
         column_names = [str(name) for name in input_data[1]]
         # Convert list to DataFrame and return
-        return convert_columns_date(pd.DataFrame(input_data[2:], columns=column_names))
+        df = convert_columns_date(pd.DataFrame(input_data[2:], columns=column_names))
+        # Drop problematic rows with all null
+        df = df.dropna(how='all', axis=0)
+        return df
     else:
         flash('Please make sure your google sheet has been published to the web.', category='error')
 
 def convert_columns_date(df):
-    '''Converts imported spreadsheet data frames into datatime.'''
+    '''Converts imported spreadsheet data frames into datetime.'''
     for index, row in df.iterrows():
         for item in row.keys()[1:]:
             if type(row[item]) is str:
