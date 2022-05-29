cfeaf5bebc496fc0a4760ca58b95df761d75b1bb
RunningMSN <running.msn.wi@gmail.com>
2022-05-29 12:34:54 -0500

    Add no schools found message.

--- website/static/js/explorer_home.js
+++ website/static/js/explorer_home.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 
 function filterSettings() {
     var all_schools = document.getElementsByClassName('school_entry');
+    var removeCount = 0;
     for (let i = 0; i < all_schools.length; i++){
         var remove = false;
         // Check school type
@@ -42,8 +43,17 @@ function filterSettings() {
         // Add or remove school
         if (remove) {
             all_schools[i].classList.add("d-none");
+            removeCount += 1;
         } else {
             all_schools[i].classList.remove("d-none");
         }
+    }
+
+    // Display no schools found message when number of schools removed matches school count
+    var noSchools = document.getElementById('no_schools');
+    if (all_schools.length == removeCount) {
+        noSchools.classList.remove("d-none");
+    } else {
+        noSchools.classList.add("d-none");
     }
 }
