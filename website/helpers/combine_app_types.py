b49e44c3a9a67daba0bd12474df23accce29a423
RunningMSN <running.msn.wi@gmail.com>
2023-08-22 14:23:08 -0500

    Updated text in combined app type visualizations.

--- website/helpers/combine_app_types.py
+++ website/helpers/combine_app_types.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def combine(data):
-    data['phd_str'] = data['phd'].apply(lambda x: '/PhD' if x else '')
-    data["name"]  = data["name"] + "_(" + data["school_type"] + data["phd_str"]+")"
+    data['phd_str'] = data['phd'].apply(lambda x: '-PhD' if x else '')
+    data["name"]  = data["name"] + " (" + data["school_type"] + data["phd_str"]+")"
     data.drop(['phd_str'],axis=1,inplace=True)
     return data
