fc3daf5bb5cf3a71bc49a6b76edeb1b145d06e69
RunningMSN <running.msn.wi@gmail.com>
2023-11-25 15:54:42 -0600

    Remove unused code.

--- website/pages.py
+++ website/pages.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template, flash, Markup, url_for
 from flask_login import current_user
 from . import db
 from .models import User, School, School_Profiles_Data
-from .visualizations import agg_map
-import pandas as pd
 
 pages = Blueprint('pages', __name__)
 
@@ -15,28 +13,19 @@ def privacy_announcement():
             current_user.privacy_announce = True
             db.session.commit()
 
-@pages.before_app_request
-def verification_required():
-    if current_user.is_authenticated:
-        if current_user.email_verified == False:
-            flash(Markup(f'Please verify your email address. If your email is not verified by November 15, 2023, your account will be deleted. <a href={url_for("authentication.resend_email",email=current_user.email)}>Resend verification email</a>.'), category='warning')
+# @pages.before_app_request
+# def verification_required():
+#     if current_user.is_authenticated:
+#         if current_user.email_verified == False:
+#             flash(Markup(f'Please verify your email address. If your email is not verified by November 30, 2023, your account will be deleted. <a href={url_for("authentication.resend_email",email=current_user.email)}>Resend verification email</a>.'), category='warning')
 
 @pages.route('/')
 def index():
     user_count = db.session.query(User.id).count()
     app_count = db.session.query(School.id).count()
     school_count = db.session.query(School).group_by(School.name).count()
-    # map_data = pd.read_sql(School.query.statement, db.get_engine()).drop(['id','cycle_id','user_id','school_type','phd'], axis=1)
-
-    # Drop empty columns
-    # map_data = map_data.dropna(axis=1, how='all')
-    # if len(map_data) > 0:
-    #     graphJSON = agg_map.generate()
-    # else:
-    #     graphJSON = None
-    graphJSON = None
     return render_template('index.html', user=current_user, user_count=user_count, school_count=school_count,
-                           app_count=app_count)#, graphJSON=graphJSON)
+                           app_count=app_count)
 
 @pages.route('/about')
 def about():
