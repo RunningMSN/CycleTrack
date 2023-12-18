cb3c98d134353d12e29335e78ab062d8a62cfdf8
RunningMSN <running.msn.wi@gmail.com>
2023-12-17 19:47:12 -0700

    Removed unnecessary prints.

--- website/helpers/import_list_funcs.py
+++ website/helpers/import_list_funcs.py
@@ -39,9 +39,7 @@ def convert_columns_date(df):
     temp_df[cols[0]] = df[cols[0]]
     '''Converts imported spreadsheet data frames into datetime.'''
     for col in cols[1:]:
-        print(df[col])
         temp_df[col] = pd.to_datetime(df[col], errors="coerce")
-        print(temp_df[col])
     return temp_df
 
 
