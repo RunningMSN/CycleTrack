d0e9a5aa5104bcfc6b5ca7fc1dcc215ada128b0a
Daniel <daniel.amusin@northwestern.edu>
2024-07-12 10:48:02 -0500

    Fixed hiding sample graphs with meet team button

--- website/static/js/about_page.js
+++ website/static/js/about_page.js
@@ -1,10 +1,16 @@
 const platform_row = document.getElementById("platform_row");
 const team_row = document.getElementById("team_row");
 const sample_list = document.getElementById("sample_list");
+const graph_card = document.getElementById("graph_card");
+const hide_btn = document.getElementById("hide_graph_button");
+var graph_buttons = document.getElementsByClassName('graph_button');
+var graphs = document.getElementsByClassName('graph');
+const sample_list_button = document.getElementById("sample_list_button");
 
 function switchToTeam() {
     team_row.classList.remove("d-none");
     platform_row.classList.add("d-none");
+    hideGraph();
 }
 
 function switchToPlatform() {
@@ -13,8 +19,6 @@ function switchToPlatform() {
 }
 
 function sampleList() {
-    const sample_list_button = document.getElementById("sample_list_button");
-
     if (sample_list.classList.contains("d-none")) {
         sample_list.classList.remove("d-none");
         sample_list_button.innerText = "Hide Sample List";
@@ -25,7 +29,6 @@ function sampleList() {
 }
 
 function displayGraph(graph_type) {
-    var graphs = document.getElementsByClassName('graph');
     for (let i = 0; i < graphs.length; i++){
         graphs[i].classList.add("d-none");
     }
@@ -33,31 +36,24 @@ function displayGraph(graph_type) {
     const graph = document.getElementById(graph_type);
     graph.classList.remove("d-none");
 
-    const graph_card = document.getElementById("graph_card");
     graph_card.classList.remove("d-none");
 
-    const hide_btn = document.getElementById("hide_graph_button");
     hide_btn.classList.remove("d-none");
 
-    var graph_buttons = document.getElementsByClassName('graph_button');
     for (let i = 0; i < graph_buttons.length; i++){
         graph_buttons[i].classList.add("d-none");
     }
 }
 
 function hideGraph() {
-    const graph_card = document.getElementById("graph_card");
     graph_card.classList.add("d-none");
 
-    const hide_btn = document.getElementById("hide_graph_button");
     hide_btn.classList.add("d-none");
 
-    var graph_buttons = document.getElementsByClassName('graph_button');
     for (let i = 0; i < graph_buttons.length; i++){
         graph_buttons[i].classList.remove("d-none");
     }
 
-    var graphs = document.getElementsByClassName('graph');
     for (let i = 0; i < graphs.length; i++){
         graphs[i].classList.add("d-none");
     }
